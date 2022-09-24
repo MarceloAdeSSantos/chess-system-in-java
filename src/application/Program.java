c6f9b13c9e58b92ae20953f62c0970314e2ee909
Marcelo Alves <Raindrop360@hotmail.com>
2022-09-24 19:23:15 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		System.out.println("Teste");
-
+		Position pos = new Position(6, 7);
+		System.out.println(pos);
 	}
-
 }
