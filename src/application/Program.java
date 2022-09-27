d8605a9640383762f095bae3a4d70a166ed3464d
Marcelo Alves <Raindrop360@hotmail.com>
2022-09-27 19:35:54 -0300

    Special move - Promotion (and another changes in UI)

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,17 +34,20 @@ public class Program {
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				
+
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-			
-			}
-			catch (ChessException e) {
+
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			}
-			catch (InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
